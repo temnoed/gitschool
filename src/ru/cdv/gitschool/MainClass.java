90f633bd98f05750af003ca51f956d0f93b4e6ea
Dmitriy <dc9257351633@gmail.com>
2017-07-28 18:30:17 +0300

    wowwow! new freedom

--- src/ru/cdv/gitschool/MainClass.java
+++ src/ru/cdv/gitschool/MainClass.java
@@ -3,7 +3,7 @@ package ru.cdv.gitschool;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("Trening gitting;;;");
+        System.out.println("Trening gitting;;;;");
     }
 
 }
